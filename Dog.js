4bddb9d0b1557c19df6274b1c99f844b86b7ab2f
nguyentronghoang96 <hoanglion.it@gmail.com>
2019-12-06 20:29:25 +0700

    add something

--- Dog.js
+++ Dog.js
@@ -1,1 +1,16 @@
-ádasd
+let chalk = require('chalk');
+
+function Dog(name) {
+    this.name = name;
+    this.stomach = []
+}
+
+Dog.prototype.eat = function(cat) {
+    this.stomach.push(cat);
+};
+
+Dog.prototype.sayHi = function() {
+    console.log('Hi! I am a dog. My name is ' + chalk.blue(this.name));
+};
+
+module.exports = Dog;
